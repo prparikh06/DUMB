981ab9cd6821f52dbffd9c1484afa5239fd41ece
prparikh06 <44184103+prparikh06@users.noreply.github.com>
2019-11-26 22:23:09 -0500

    geeks

--- DUMBserver.c
+++ DUMBserver.c
@@ -10,49 +10,53 @@
 //server socket port number: random number between 4096 and 65k
 
 int main(int argc, char** argv){
+	int sockfd, connfd, len; 
+    struct sockaddr_in servaddr, cli; 
+  
+    // socket create and verification 
+    sockfd = socket(AF_INET, SOCK_STREAM, 0); 
+    if (sockfd == -1) { 
+        printf("socket creation failed...\n"); 
+        exit(0); 
+    } 
+    else
+        printf("Socket successfully created..\n"); 
+    bzero(&servaddr, sizeof(servaddr)); 
+  
+    // assign IP, PORT 
+    servaddr.sin_family = AF_INET; 
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY); 
+    servaddr.sin_port = htons(PORT); 
+  
+    // Binding newly created socket to given IP and verification 
+    if ((bind(sockfd, (SA*)&servaddr, sizeof(servaddr))) != 0) { 
+        printf("socket bind failed...\n"); 
+        exit(0); 
+    } 
+    else
+        printf("Socket successfully binded..\n"); 
+  
+    // Now server is ready to listen and verification 
+    if ((listen(sockfd, 5)) != 0) { 
+        printf("Listen failed...\n"); 
+        exit(0); 
+    } 
+    else
+        printf("Server listening..\n"); 
+    len = sizeof(cli); 
+  
+    // Accept the data packet from client and verification 
+    connfd = accept(sockfd, (SA*)&cli, &len); 
+    if (connfd < 0) { 
+        printf("server acccept failed...\n"); 
+        exit(0); 
+    } 
+    else
+        printf("server acccept the client...\n"); 
+  
 
-	int sockfd, connectionfd;
-	struct sockaddr_in server_addy, client;	
 
 
-
-	int port_num = atoi(argv[1]);
-	
-	//TODO listen and accept here
-	sockfd = socket(AF_INET,SOCK_STREAM,0);
-	if(sockfd == -1){
-		printf("error");
-		return 0;
-	}
-
-	bzero(&server_addy, sizeof(server_addy));
-	
-	server_addy.sin_family = AF_INET;
-	server_addy.sin_addr.s_addr = htonl(INADDR_ANY);
-	server_addy.sin_port = htons(port_num);
-
-	if (bind(sockfd, (struct sockaddr*)&server_addy,sizeof( server_addy)) != 0){
-
-		printf("error in binding\n");
-		return 0;
-	}
-	if (listen (sockfd, 3) != 0){
-
-		printf("error in listening\n");
-		return 0;
-	}
-	
-	int size = sizeof(struct sockaddr_in);
-	
-	connectionfd = accept(sockfd, (struct sockaddr*) &client,(socklen_t*) &size );
-	if (connectionfd < 0) {
-		printf("error in accepting\n");
-		return 0;
-	}
-
-	printf("ayyy the server accepted!!\n");
-
-	
 
 	return 0;	
 
