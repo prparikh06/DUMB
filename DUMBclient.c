cb6ecc36d3dbeb73c4595e82af2a5f1a750a2fb6
Priya Parikh <pp649@assembly.cs.rutgers.edu>
2019-11-26 22:21:37 -0500

    testing broken

--- DUMBclient.c
+++ DUMBclient.c
@@ -1,9 +1,14 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <errno.h>
+#include <netdb.h> 
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <string.h> 
 #include <sys/socket.h>
 
+#include <sys/types.h> 
+#include <netinet/in.h> 
+
+
+//52613
 int numAttempts = 0;
 char* commandOptions[] = {"quit", "create", "delete", "open", "close", "next", "put"};
 
@@ -19,13 +24,57 @@ int checkCommand(char* str){
 }
 
 
-int main(int argc, char** argv ){
+int main(int argc, char* argv[] ){
 	
 	char* hostname = argv[1];
 	int port_num = atoi(argv[2]);
 	
 	//TODO connect to server here
-		
+	
+	int sockfd = socket(AF_INET, SOCK_STREAM,0);
+	if (sockfd < 0){ //TODO Error
+		printf("error\n");
+		return 0;
+	}
+	
+	char* ip;
+	struct hostent *host = gethostbyname(hostname);
+	
+	if (host == NULL){ //TODO error
+		printf("hostname error\n");
+		return 0;
+
+	}
+	
+	struct sockaddr_in server_addy;
+	bzero(&server_addy, sizeof(server_addy));
+	
+	
+
+	server_addy.sin_addr = *(struct in_addr *) host->h_addr_list;
+
+
+	server_addy.sin_family = AF_INET;
+	//server_addy.sin_addr.s_addr = inet_addr(ip);
+	//server_addy.sin_addr = *((struct in_addr *) host->h_addr_list[0]);
+	//memcpy(&(server_addy.sin_addr.s_addr), host->h_addr_list[0], host->h_length);
+	server_addy.sin_port = htons(port_num);
+	
+	if(connect(sockfd, (struct sockaddr *) &server_addy, sizeof(server_addy)) < 0){
+		printf("could not be connected :(\n");
+		return 0;
+	}
+	
+	
+	
+	//printf("connected!!!!\n");
+
+	
+
+	//TODO do stuff
+
+	
+	/*	
 	//accpet some commands (HELLO)
 	printf("HELLO!\n");
 	int acceptCommands = 1;
@@ -35,14 +84,11 @@ int main(int argc, char** argv ){
 		if (checkCommand(command) != 1)
 			acceptCommands = 0;
 		
-
 		printf("command is %s\n", command);
-		
-
 
 	}
 	printf("exitted loop\n");	
-
+	*/
 	return 0;
 
 }
--- DUMBserver.c
+++ DUMBserver.c
@@ -1,18 +1,59 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <errno.h>
-#include <sys/socket.h>
+#include <stdio.h> 
+#include <netdb.h> 
+#include <netinet/in.h> 
+#include <stdlib.h> 
+#include <string.h> 
+#include <sys/socket.h> 
+#include <sys/types.h> 
 
 
 //server socket port number: random number between 4096 and 65k
 
 int main(int argc, char** argv){
 
+	int sockfd, connectionfd;
+	struct sockaddr_in server_addy, client;	
+
+
+
 	int port_num = atoi(argv[1]);
 	
 	//TODO listen and accept here
+	sockfd = socket(AF_INET,SOCK_STREAM,0);
+	if(sockfd == -1){
+		printf("error");
+		return 0;
+	}
+
+	bzero(&server_addy, sizeof(server_addy));
 	
+	server_addy.sin_family = AF_INET;
+	server_addy.sin_addr.s_addr = htonl(INADDR_ANY);
+	server_addy.sin_port = htons(port_num);
+
+	if (bind(sockfd, (struct sockaddr*)&server_addy,sizeof( server_addy)) != 0){
+
+		printf("error in binding\n");
+		return 0;
+	}
+	if (listen (sockfd, 3) != 0){
+
+		printf("error in listening\n");
+		return 0;
+	}
+	
+	int size = sizeof(struct sockaddr_in);
+	
+	connectionfd = accept(sockfd, (struct sockaddr*) &client,(socklen_t*) &size );
+	if (connectionfd < 0) {
+		printf("error in accepting\n");
+		return 0;
+	}
+
+	printf("ayyy the server accepted!!\n");
+
+	
+
 	return 0;	
 
 
