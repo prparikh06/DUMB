80bd0e3d5ecb1d9f7b76558762dc03962ece5bc0
Mazaya Rahman <mr1411@cpp.cs.rutgers.edu>
2019-11-28 19:39:06 -0500

    server talks

--- DUMBclient.c
+++ DUMBclient.c
@@ -1,9 +1,9 @@
-#include <stdio.h> 
-#include <stdlib.h> 
-#include <string.h> 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/socket.h>
-#include <sys/types.h> 
-#include <netinet/in.h> 
+#include <sys/types.h>
+#include <netinet/in.h>
 #include <unistd.h>
 #include <pthread.h>
 #include <netdb.h>
@@ -24,27 +24,27 @@ int checkCommand(char* str){
 
 }
 
-void func(int sockfd) 
-{ 
-    char buff[100]; 
-    int n; 
-    for (;;) { 
-        bzero(buff, sizeof(buff)); 
-        printf("Enter the string : "); 
-        n = 0; 
-        while ((buff[n++] = getchar()) != '\n') 
-            ; 
-        write(sockfd, buff, sizeof(buff)); 
-        bzero(buff, sizeof(buff)); 
-        read(sockfd, buff, sizeof(buff)); 
-        printf("From Server : %s", buff); 
-        if ((strncmp(buff, "exit", 4)) == 0) { 
-            printf("Client Exit...\n"); 
-            break; 
-        } 
-    } 
-} 
-  
+void func(int sockfd)
+{
+    char buff[100];
+    int n;
+    for (;;) {
+        bzero(buff, sizeof(buff));
+        printf("Enter the string : ");
+        n = 0;
+        while ((buff[n++] = getchar()) != '\n')
+            ;
+        write(sockfd, buff, sizeof(buff));
+        bzero(buff, sizeof(buff));
+        read(sockfd, buff, sizeof(buff));
+        printf("From Server : %s", buff);
+        if ((strncmp(buff, "exit", 4)) == 0) {
+            printf("Client Exit...\n");
+            break;
+        }
+    }
+}
+
 
 void readCommands(int sockfd){
 
@@ -56,48 +56,52 @@ void readCommands(int sockfd){
 		printf("message is: %s\n", message);
 		write(sockfd, message,sizeof(message));
 		if (strcmp(message,"exit") == 0) break;
-		
+		bzero(message,sizeof(message));
+		read(sockfd,message,sizeof(message));
+		printf("client sent message: %s\n", message);
+		if (strcmp(message, "exit") == 0) break;
+
 	}
 }
 
 
 
-int main(int argc, char* argv[]) { 
-    int sockfd, connfd; 
-    struct sockaddr_in servaddr, cli; 
+int main(int argc, char* argv[]) {
+    int sockfd, connfd;
+    struct sockaddr_in servaddr, cli;
 
 	char* hostname = argv[1];
 	int port_num = atoi(argv[2]);
 
-    // socket create and varification 
-    sockfd = socket(AF_INET, SOCK_STREAM, 0); 
-    if (sockfd == -1) { 
+    // socket create and varification
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd == -1) {
         return 0;
-    } 
-    
-    bzero(&servaddr, sizeof(servaddr)); 
-  
+    }
+
+    bzero(&servaddr, sizeof(servaddr));
+
   	struct hostent *host = gethostbyname(hostname);
-	
+
 	if (host == NULL){ //TODO error
 		printf("hostname error\n");
 		return 0;
 	}
-    // assign IP, PORT 
-    servaddr.sin_family = AF_INET; 
+    // assign IP, PORT
+    servaddr.sin_family = AF_INET;
 	servaddr.sin_addr = *(struct in_addr* )* host->h_addr_list;
-    servaddr.sin_port = htons(port_num); 
-  
-    // connect the client socket to server socket 
-    if (connect(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr)) != 0) { 
-        printf("connecting failed :(\n"); 
-        exit(0); 
-    } 
+    servaddr.sin_port = htons(port_num);
+
+    // connect the client socket to server socket
+    if (connect(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr)) != 0) {
+        printf("connecting failed :(\n");
+        exit(0);
+    }
     else
-        printf("connected!!!\n"); 
-  
-    readCommands(sockfd); 
-  
-    close(sockfd); 
-} 
+        printf("connected!!!\n");
+
+    readCommands(sockfd);
+
+    close(sockfd);
+}
 
--- DUMBserver.c
+++ DUMBserver.c
@@ -1,41 +1,41 @@
-#include <stdio.h> 
-#include <netdb.h> 
-#include <netinet/in.h> 
-#include <stdlib.h> 
-#include <string.h> 
-#include <sys/socket.h> 
-#include <sys/types.h> 
+#include <stdio.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
 
 //server socket port number: random number between 4096 and 65k
 
-void func(int sockfd) 
-{ 
-    char buff[100]; 
-    int n; 
-    // infinite loop for chat 
-    for (;;) { 
-        bzero(buff, 100); 
-  
-        // read the message from client and copy it in buffer 
-        read(sockfd, buff, sizeof(buff)); 
-        // print buffer which contains the client contents 
-        printf("From client: %s\t To client : ", buff); 
-        bzero(buff, 100); 
-        n = 0; 
-        // copy server message in the buffer 
-        while ((buff[n++] = getchar()) != '\n') 
-            ; 
-  
-        // and send that buffer to client 
-        write(sockfd, buff, sizeof(buff)); 
-  
-        // if msg contains "Exit" then server exit and chat ended. 
-        if (strncmp("exit", buff, 4) == 0) { 
-            printf("Server Exit...\n"); 
-            break; 
-        } 
-    } 
-} 
+void func(int sockfd)
+{
+    char buff[100];
+    int n;
+    // infinite loop for chat
+    for (;;) {
+        bzero(buff, 100);
+
+        // read the message from client and copy it in buffer
+        read(sockfd, buff, sizeof(buff));
+        // print buffer which contains the client contents
+        printf("From client: %s\t To client : ", buff);
+        bzero(buff, 100);
+        n = 0;
+        // copy server message in the buffer
+        while ((buff[n++] = getchar()) != '\n')
+            ;
+
+        // and send that buffer to client
+        write(sockfd, buff, sizeof(buff));
+
+        // if msg contains "Exit" then server exit and chat ended.
+        if (strncmp("exit", buff, 4) == 0) {
+            printf("Server Exit...\n");
+            break;
+        }
+    }
+}
 
 
 void interpretCommands(int connfd){
@@ -45,7 +45,14 @@ void interpretCommands(int connfd){
 		read(connfd,message,sizeof(message));
 		printf("client sent message: %s\n", message);
 		if (strcmp(message, "exit") == 0) break;
-		
+		bzero(message,sizeof(message));
+		printf("Enter a message...\n");
+		scanf("%s", message);
+		printf("message is: %s\n", message);
+		write(connfd, message,sizeof(message));
+		if (strcmp(message,"exit") == 0) break;
+
+
 	}
 
 }
@@ -53,52 +60,52 @@ void interpretCommands(int connfd){
 
 
 
-int main(int argc, char* argv[]) { 
-    int sockfd, connfd; 
-    struct sockaddr_in servaddr, clientaddr; 
+int main(int argc, char* argv[]) {
+    int sockfd, connfd;
+    struct sockaddr_in servaddr, clientaddr;
 
     int port_num = atoi(argv[1]);
-  
-    // socket create and verification 
-    sockfd = socket(AF_INET, SOCK_STREAM, 0); 
-    if (sockfd == -1) { 
-       return 0; 
-    } 
-   
-    bzero(&servaddr, sizeof(servaddr)); 
-  
-    // assign IP, PORT 
-    servaddr.sin_family = AF_INET; 
-    servaddr.sin_addr.s_addr = htonl(INADDR_ANY); 
-    servaddr.sin_port = htons(port_num); 
-  
-    // Binding newly created socket to given IP and verification 
-    if ((bind(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr))) != 0) { 
-        printf("binding failed\n"); 
-        return 0; 
-    } 
-    
-    // Now server is ready to listen and verification 
-    if ((listen(sockfd, 5)) != 0) { 
-        printf("Listening failed...\n"); 
-        return 0;
-    } 
-    else
-        printf("Listening..\n"); 
 
-    int size = sizeof(clientaddr); 
-  
-    // Accept the data packet from client and verification 
-    connfd = accept(sockfd, (struct sockaddr*)&clientaddr, &size); 
-    if (connfd < 0) { 
-        printf("could not accept client :(\n"); 
-        return 0;
-    } 
-    else
-        printf("accepted!!!\n"); 
-  
+    // socket create and verification
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd == -1) {
+       return 0;
+    }
 
-   interpretCommands(connfd); 
- 
-   close(sockfd); 
-} 
+    bzero(&servaddr, sizeof(servaddr));
+
+    // assign IP, PORT
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servaddr.sin_port = htons(port_num);
+
+    // Binding newly created socket to given IP and verification
+    if ((bind(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr))) != 0) {
+        printf("binding failed\n");
+        return 0;
+    }
+
+    // Now server is ready to listen and verification
+    if ((listen(sockfd, 5)) != 0) {
+        printf("Listening failed...\n");
+        return 0;
+    }
+    else
+        printf("Listening..\n");
+
+    int size = sizeof(clientaddr);
+
+    // Accept the data packet from client and verification
+    connfd = accept(sockfd, (struct sockaddr*)&clientaddr, &size);
+    if (connfd < 0) {
+        printf("could not accept client :(\n");
+        return 0;
+    }
+    else
+        printf("accepted!!!\n");
+
+
+   interpretCommands(connfd);
+
+   close(sockfd);
+}
